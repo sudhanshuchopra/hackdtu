1f3eaffc6a0a564c26753957d027b5e0413cd2eb
sudhanshu <sudhanshuccp@gmail.com>
2017-02-10 23:44:33 +0530

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class MyUser(AbstractUser):
+	phone=models.CharField(max_length=10,null=True)
+	adhaar_card = models.BigIntegerField()
+	profile_pic=models.ImageField(upload_to='profile_pics/',null=True,blank=True)
+	blood_group = models.CharField(max_length=3,null = True,blank = True)
+
